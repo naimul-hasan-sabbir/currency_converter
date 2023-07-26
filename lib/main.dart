a3473d7c39e94acee3d9ebdf69b8453d3377e640
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-07-26 23:11:42 +0600

    learned about scaffold

--- lib/main.dart
+++ lib/main.dart
@@ -9,9 +9,11 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Text(
-      'Hello Naimul',
-      textDirection: TextDirection.rtl,
+    return MaterialApp(
+      home: Scaffold(
+          body: Center(
+        child: Text("Hello World!!!"),
+      )),
     );
   }
 }
