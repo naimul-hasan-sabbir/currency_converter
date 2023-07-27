380f2f348d85683326c523cb8807ff04cd4f05ea
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-07-27 23:47:03 +0600

    splitted widgets

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import './currency_converter_material_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -10,10 +11,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: Scaffold(
-          body: Center(
-        child: Text("Hello World!!!"),
-      )),
+      home: CurrencyConverterMaterialPage(),
     );
   }
 }
