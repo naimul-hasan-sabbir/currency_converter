9751a41783c0deafcf74709100b401bc15854031
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-07-31 22:35:36 +0600

    updated

--- lib/pages/currency_converter_material_page.dart
+++ lib/pages/currency_converter_material_page.dart
@@ -5,14 +5,15 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ColoredBox(color: Color.fromRGBO(255, 0, 0, 1),)
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          crossAxisAlignment: CrossAxisAlignment.center,
-          children: [
-            Text('0'),
-          ],
-        ),
-    );
+        body: ColoredBox(
+      color: Color.fromRGBO(255, 0, 0, 1),
+      child: Column(
+        mainAxisAlignment: MainAxisAlignment.center,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Text('0'),
+        ],
+      ),
+    ));
   }
 }
