9f55b46796bbda25045514e1f6c848b7b3fb7be3
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-08-04 13:39:44 +0600

    color property added

--- lib/pages/currency_converter_material_page.dart
+++ lib/pages/currency_converter_material_page.dart
@@ -5,19 +5,21 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+        backgroundColor: Color.fromRGBO(0, 0, 0, 1),
         body: ColoredBox(
-      color: Color.fromRGBO(255, 0, 0, 1),
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        crossAxisAlignment: CrossAxisAlignment.center,
-        children: [
-          Text('0',
-              style: TextStyle(
-                fontSize: 45,
-                fontWeight: FontWeight.bold,
-              )),
-        ],
-      ),
-    ));
+          color: Color.fromRGBO(255, 0, 0, 1),
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            crossAxisAlignment: CrossAxisAlignment.center,
+            children: [
+              Text('0',
+                  style: TextStyle(
+                    fontSize: 45,
+                    fontWeight: FontWeight.bold,
+                    color: Color.fromARGB(255, 255, 255, 255),
+                  )),
+            ],
+          ),
+        ));
   }
 }
