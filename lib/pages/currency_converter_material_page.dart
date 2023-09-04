73f417a9cfcea8d854ade358848f258a852cfc13
naimul-hasan-sabbir <54884408+naimul-hasan-sabbir@users.noreply.github.com>
2023-09-04 23:56:14 +0600

    started converting currency

--- lib/pages/currency_converter_material_page.dart
+++ lib/pages/currency_converter_material_page.dart
@@ -15,71 +15,72 @@ class CurrencyConverterMaterialPage extends StatelessWidget {
       borderRadius: BorderRadius.all(Radius.circular(60)),
     );
     return Scaffold(
+      backgroundColor: Colors.blueGrey,
+      appBar: AppBar(
         backgroundColor: Colors.blueGrey,
-        appBar: AppBar(
-          backgroundColor: Colors.blueGrey,
-          elevation: 0,
-          title: Text("Currency Converter"),
-          centerTitle: true,
-        ),
-        
-          //color: Color.fromRGBO(255, 0, 0, 1),
-          child: Column(
-            mainAxisAlignment: MainAxisAlignment.center,
-            crossAxisAlignment: CrossAxisAlignment.center,
-            children: [
-              Text(
-                '0',
-                style: TextStyle(
-                  fontSize: 45,
-                  fontWeight: FontWeight.bold,
-                  color: Color.fromARGB(255, 255, 255, 255),
-                ),
+        elevation: 0,
+        title: Text("Currency Converter"),
+        centerTitle: true,
+      ),
+      body: Center(
+        //color: Color.fromRGBO(255, 0, 0, 1),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Text(
+              '0',
+              style: TextStyle(
+                fontSize: 45,
+                fontWeight: FontWeight.bold,
+                color: Color.fromARGB(255, 255, 255, 255),
               ),
-              Container(
-                padding: const EdgeInsets.all(10),
-                child: TextField(
-                  style: TextStyle(
+            ),
+            Container(
+              padding: const EdgeInsets.all(10),
+              child: TextField(
+                style: TextStyle(
+                  color: Colors.black,
+                ),
+                decoration: InputDecoration(
+                  hintText: 'Please enter the amount in BDT',
+                  hintStyle: TextStyle(
                     color: Colors.black,
                   ),
-                  decoration: InputDecoration(
-                    hintText: 'Please enter the amount in BDT',
-                    hintStyle: TextStyle(
-                      color: Colors.black,
-                    ),
-                    prefixIcon: Icon(Icons.monetization_on_outlined),
-                    prefixIconColor: Colors.black,
-                    filled: true,
-                    fillColor: Colors.white,
-                    focusedBorder: border,
-                    enabledBorder: border,
-                  ),
-                  keyboardType: const TextInputType.numberWithOptions(
-                    decimal: true,
-                  ),
+                  prefixIcon: Icon(Icons.monetization_on_outlined),
+                  prefixIconColor: Colors.black,
+                  filled: true,
+                  fillColor: Colors.white,
+                  focusedBorder: border,
+                  enabledBorder: border,
+                ),
+                keyboardType: const TextInputType.numberWithOptions(
+                  decimal: true,
                 ),
               ),
-              Padding(
-                padding: const EdgeInsets.all(10.0),
-                child: ElevatedButton(
-                  onPressed: () {
-                    if (kDebugMode) {
-                      print('button clicked');
-                    }
-                  },
-                  style: TextButton.styleFrom(
-                    backgroundColor: Colors.black,
-                    foregroundColor: Colors.white,
-                    fixedSize: const Size(double.infinity, 50),
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10),
-                    ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: ElevatedButton(
+                onPressed: () {
+                  if (kDebugMode) {
+                    print('button clicked');
+                  }
+                },
+                style: TextButton.styleFrom(
+                  backgroundColor: Colors.black,
+                  foregroundColor: Colors.white,
+                  fixedSize: const Size(double.infinity, 50),
+                  shape: RoundedRectangleBorder(
+                    borderRadius: BorderRadius.circular(10),
                   ),
-                  child: const Text('Click Me'),
                 ),
-              )
-            ],
-          ),
-        ));
+                child: const Text('Click Me'),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
